b3e90f572f784705a71da30650277dccea2ce1f1
SHUNSUKESANO <avoshunsuke@gmail.com>
2021-04-21 14:41:20 +0900

    order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @activities = @user.activities.all
+    @activities = @user.activities.all.order(created_at: :desc)
   end
 
   private 
