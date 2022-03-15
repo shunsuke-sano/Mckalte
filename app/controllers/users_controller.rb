fd9a6bbfcb5df760ae313bc7774ae3404bbbad03
SHUNSUKESANO <avoshunsuke@gmail.com>
2022-03-15 19:44:00 +0900

    a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   private 
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :grade)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :grade, :admin)
   end
 
   
